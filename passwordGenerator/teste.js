7916f6a1218bd5d4c26a2ef6c2c14d2c601f7a7d
Julio Cruz <juliocesarlc@outlook.com>
2023-08-22 19:33:04 -0300

    corrigindo js

--- passwordGenerator/teste.js
+++ passwordGenerator/teste.js
@@ -1,10 +1,10 @@
-// Clear the concole on every refresh
+//clear the console on every refresh 
 console.clear();
 
-// Range Slider Properties.
+//range slider properties
 const sliderProps = {
-	fill: "#0B1EDF",
-	background: "rgba(255, 255, 255, 0.214)",
+    fill: "#d19316",
+    background: "rgba(255, 255, 255, 0.214)",
 };
 
 const slider = document.querySelector(".range__slider");
@@ -12,45 +12,45 @@ const slider = document.querySelector(".range__slider");
 const sliderValue = document.querySelector(".length__title");
 
 slider.querySelector("input").addEventListener("input", event => {
-	sliderValue.setAttribute("data-length", event.target.value);
-	applyFill(event.target);
+    sliderValue.setAttribute("data-length", event.target.value);
+    applyFill(event.target);
 });
 
 applyFill(slider.querySelector("input"));
 
 function applyFill(slider) {
-	const percentage = (100 * (slider.value - slider.min)) / (slider.max - slider.min);
-	const bg = `linear-gradient(90deg, ${sliderProps.fill} ${percentage}%, ${sliderProps.background} ${percentage +
-			0.1}%)`;
-	slider.style.background = bg;
-	sliderValue.setAttribute("data-length", slider.value);
+    const percentage = (100 * (slider.value - slider.min)) / (slider.max - slider.min);
+    const bg = `linear-gradient(90deg, ${sliderProps.fill} ${percentage}%, ${sliderProps.background} ${percentage + 
+            0.1}%)`;
+    slider.style.background = bg;
+    sliderValue.setAttribute("data-length", slider.value);
 }
 
 const randomFunc = {
-	lower: getRandomLower,
-	upper: getRandomUpper,
-	number: getRandomNumber,
-	symbol: getRandomSymbol,
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol,
 };
 
-// Random more secure value
+//Random more secure value
 function secureMathRandom() {
-	return window.crypto.getRandomValues(new Uint32Array(1))[0] / (Math.pow(2, 32) - 1);
+    return window.crypto.getRandomValues(new Uint32Array(1))[0] / (Math.pow(2, 32) - 1);
 }
 
-// Generator Functions
+//generator functions
 function getRandomLower() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 function getRandomUpper() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 function getRandomNumber() {
-	return String.fromCharCode(Math.floor(secureMathRandom() * 10) + 48);
+    return String.fromCharCode(Math.floor(secureMathrandom() * 10) + 48);//(Math.random)
 }
 function getRandomSymbol() {
-	const symbols = '~!@#$%^&*()_+{}":?><;.,';
-	return symbols[Math.floor(Math.random() * symbols.length)];
+    const symbols = '~!@#$%^&*()_+{}":?><;.,';
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
 const resultEl = document.getElementById("result");
@@ -61,7 +61,7 @@ const lowercaseEl = document.getElementById("lowercase");
 const numberEl = document.getElementById("number");
 const symbolEl = document.getElementById("symbol");
 
-// Button to generate the password
+//button to generate the password
 const generateBtn = document.getElementById("generate");
 
 const copyBtn = document.getElementById("copy-btn");
@@ -75,96 +75,96 @@ const copiedInfo = document.querySelector(".result__info.left");
 let generatedPassword = false;
 
 let resultContainerBound = {
-	left: resultContainer.getBoundingClientRect().left,
-	top: resultContainer.getBoundingClientRect().top,
+    left: resultContainer.getBoundingClientRect().left,
+    top: resultContainer.getBoundingClientRect().top,
 };
 
 resultContainer.addEventListener("mousemove", e => {
-	resultContainerBound = {
-		left: resultContainer.getBoundingClientRect().left,
-		top: resultContainer.getBoundingClientRect().top,
-	};
-	if(generatedPassword){
-		copyBtn.style.opacity = '1';
-		copyBtn.style.pointerEvents = 'all';
-		copyBtn.style.setProperty("--x", `${e.x - resultContainerBound.left}px`);
-		copyBtn.style.setProperty("--y", `${e.y - resultContainerBound.top}px`);
-	}else{
-		copyBtn.style.opacity = '0';
-		copyBtn.style.pointerEvents = 'none';
-	}
+    resultContainerBound = {
+        left: resultContainer.getBoundingClientRect().left,
+        top: resultContainer.getBoundingClientRect().top,
+    };
+    if(generatedPassword){
+        copyBtn.style.opacity = '1';
+        copyBtn.style.pointerEvents = 'all';
+        copyBtn.style.setProperty("--x", `${e.x - resultContainerBound.left}px`);
+        copyBtn.style.setProperty("--y", `${e.y - resultContainerBound.top}px`);
+    }else {
+        copyBtn.style.opacity = '0';
+        copyBtn.style.pointerEvents = 'none';
+    } 
 });
 window.addEventListener("resize", e => {
-	resultContainerBound = {
-		left: resultContainer.getBoundingClientRect().left,
-		top: resultContainer.getBoundingClientRect().top,
-	};
+    resultContainerBound = {
+        left: resultContainer.getBoundingClientRect().left,
+        top: resultContainer.getBoundingClientRect().top,
+    };
 });
 
-// Copy Password in clipboard
+//copy password clipboard
 copyBtn.addEventListener("click", () => {
-	const textarea = document.createElement("textarea");
-	const password = resultEl.innerText;
-	if (!password || password == "CLICK GENERATE") {
-		return;
-	}
-	textarea.value = password;
-	document.body.appendChild(textarea);
-	textarea.select();
-	document.execCommand("copy");
-	textarea.remove();
+    const textarea = document.createElement("textarea");
+    const password = resultEl.innerText;
+    if (!password || password == "CLICK GENERATE") {
+        return;
+    }
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
 
-	copyInfo.style.transform = "translateY(200%)";
-	copyInfo.style.opacity = "0";
-	copiedInfo.style.transform = "translateY(0%)";
-	copiedInfo.style.opacity = "0.75";
+    copyInfo.style.transform = "translateY(200%)";
+    copyInfo.style.opacity = "0";
+    copiedInfo.style.transform = "translateY(0%)";
+    copiedInfo.style.opacity = "0.75";
 });
 
 generateBtn.addEventListener("click", () => {
-	const length = +lengthEl.value;
-	const hasLower = lowercaseEl.checked;
-	const hasUpper = uppercaseEl.checked;
-	const hasNumber = numberEl.checked;
-	const hasSymbol = symbolEl.checked;
-	generatedPassword = true;
-	resultEl.innerText = generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol);
-	copyInfo.style.transform = "translateY(0%)";
-	copyInfo.style.opacity = "0.75";
-	copiedInfo.style.transform = "translateY(200%)";
-	copiedInfo.style.opacity = "0";
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numberEl.checked;
+    const hasSymbol = symbolEl.checked;
+    generatedPassword = true;
+    resultEl.innerText = generatedPassword(length, hasLower, hasUpper, hasNumber, hasSymbol);
+    copyInfo.style.transform = "translateY(0%)";
+    copyInfo.style.opacity = "0.75";
+    copiedInfo.style.transform = "translateY(200%)";
+    copiedInfo.style.opacity = "0";
 });
 
-function generatePassword(length, lower, upper, number, symbol) {
-	let generatedPassword = "";
-	const typesCount = lower + upper + number + symbol;
-	const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
-	if (typesCount === 0) {
-		return "";
-	}
-	for (let i = 0; i < length; i++) {
-		typesArr.forEach(type => {
-			const funcName = Object.keys(type)[0];
-			generatedPassword += randomFunc[funcName]();
-		});
-	}
-	return generatedPassword.slice(0, length)
-									.split('').sort(() => Math.random() - 0.5)
-									.join('');
+function generatedPassword(length, lower, upper, number, symbol) {
+    let generatedPassword = "";
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+    if (typesCount === 0) {
+        return "";
+    }
+    for (let i = 0; i < length; i++) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+    return generatedPassword.slice(0, length)
+                                    .split('').sort(() => Math.random() - 0.5)
+                                    .join('');
 }
 
 function disableOnlyCheckbox(){
-	let totalChecked = [uppercaseEl, lowercaseEl, numberEl, symbolEl].filter(el => el.checked)
-	totalChecked.forEach(el => {
-		if(totalChecked.length == 1){
-			el.disabled = true;
-		}else{
-			el.disabled = false;
-		}
-	})
+    let totalChecked = [uppercaseEl, lowercaseEl, numberEl, symbolEl].filter(el => el.checked)
+    totalChecked.forEach(el => {
+        if(totalChecked.length == 1){
+            el.disabled = true;
+        }else{
+            el.disabled = false;
+        }
+    })
 }
 
 [uppercaseEl, lowercaseEl, numberEl, symbolEl].forEach(el => {
-	el.addEventListener('click', () => {
-		disableOnlyCheckbox()
-	})
+    el.addEventListener('click', () => {
+        disableOnlyCheckbox()
+    })
 })
